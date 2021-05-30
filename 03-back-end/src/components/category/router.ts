4f6687f38b3ec72c9360215cbfaeba18c486a578
Luka Gajić <45518706+lukagajic@users.noreply.github.com>
2021-05-30 14:50:11 +0200

    Implement a middleware to verifty JWT tokens in requests

--- 03-back-end/src/components/category/router.ts
+++ 03-back-end/src/components/category/router.ts
@@ -2,12 +2,18 @@ import * as express from 'express';
 import CategoryController from './controller';
 import IApplicationResources from '../../common/IApplicationResources.interface';
 import IRouter from '../../common/IRouter.inteface';
+import AuthMiddleware from '../../middleware/auth.middleware';
 
 export default class CategoryRouter implements IRouter {
     public setupRoutes(application: express.Application, resources: IApplicationResources) {
         const categoryController: CategoryController = new CategoryController(resources);
 
-        application.get("/category",        categoryController.getAll.bind(categoryController));
+        application.get(
+            "/category",        
+            AuthMiddleware.getVerifier("administrator", "doctor"),
+            categoryController.getAll.bind(categoryController)
+        );
+
         application.get("/category/:id",    categoryController.getById.bind(categoryController));
         application.post("/category",       categoryController.add.bind(categoryController));
         application.put("/category/:id",    categoryController.edit.bind(categoryController));
---
+++ 03-back-end/src/middleware/auth.middleware.ts
@@ -0,0 +1,82 @@
+import * as express from 'express';
+import * as jwt from "jsonwebtoken";
+import Config from '../config/dev';
+import ITokenData from '../components/auth/dto/ITokenData.interface';
+
+type UserRole = "administrator" | "doctor";
+
+interface TokenValidationInfo {
+    isValid: boolean;
+    data: any;
+}
+
+export default class AuthMiddleware {
+
+    private static verifyAuthToken(req: express.Request, res: express.Response, next: express.NextFunction, allowedRoles: UserRole[]) {
+        if (typeof req.headers.authorization !== "string") {
+            return res.status(401).send("No auth token specified");
+        }
+
+        const token: string = req.headers.authorization;
+        const [ tokenType, tokenString ] = token.trim().split(" ");
+
+        if (tokenType !== "Bearer") {
+            return res.status(400).send("Invalid auth token type specified");
+        }
+
+        if (typeof tokenString !== "string" || tokenString.length === 0) {
+            return res.status(400).send("Invalid auth token length");
+        }
+
+        const doctorTokenValidation = this.validateTokenByRole(tokenString, "doctor");
+        const administratorTokenValidation = this.validateTokenByRole(tokenString, "administrator");
+
+        let result;
+
+        if (doctorTokenValidation.isValid === false && administratorTokenValidation.isValid === false) {
+            return res.status(500).send("Token validation error: " + doctorTokenValidation.data + " " + administratorTokenValidation.data);
+        }
+
+        if (doctorTokenValidation.isValid) {
+            result = doctorTokenValidation.data;
+        } else {
+            result = administratorTokenValidation.data;
+        }
+
+        if (typeof result !== "object") {
+            return res.status(400).send("Bad auth token data");
+        }
+
+        const data: ITokenData = result as ITokenData;
+
+        if (!allowedRoles.includes(data.role)) {
+            return res.status(403).send("Access denied to this role");
+        }
+
+        req.authorized = data;
+
+        next();
+    }
+
+    private static validateTokenByRole(tokenString: string, role: UserRole): TokenValidationInfo {
+        try {
+            const result = jwt.verify(tokenString, Config.auth[role].auth.public);
+            return {
+                isValid: true,
+                data: result
+            };
+        } catch (e) {
+            return {
+                isValid: false,
+                data: e?.message
+            };
+        }
+    }
+
+    public static getVerifier(...allowedRoles: UserRole[]):
+        (req: express.Request, res: express.Response, next: express.NextFunction) => void {
+            return (req: express.Request, res: express.Response, next: express.NextFunction) => {
+                this.verifyAuthToken(req, res, next, allowedRoles);
+            };
+        }
+}
