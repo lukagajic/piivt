3b53668f672e48ce028c1e84ffaebf8f5274a367
Luka Gajić <45518706+lukagajic@users.noreply.github.com>
2021-05-30 15:18:05 +0200

    Initialize a new React project

---
+++ 04-front-end/src/components/Application/Application.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Application.css';
+
+function Application() {
+  return (
+    <div className="Application">
+      ...
+    </div>
+  );
+}
+
+export default Application;
