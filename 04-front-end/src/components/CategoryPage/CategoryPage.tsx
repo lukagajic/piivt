68e51e319d38d71e9ab38a35afa6d34de8bdafd9
Luka Gajić <45518706+lukagajic@users.noreply.github.com>
2021-05-31 01:20:21 +0200

    Implement real api communication to fetch category data

--- 04-front-end/src/components/CategoryPage/CategoryPage.tsx
+++ 04-front-end/src/components/CategoryPage/CategoryPage.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 import BasePage from '../BasePage/BasePage';
+import axios from "axios";
+import CategoryModel from '../../../../03-back-end/src/components/category/model';
+import { Link } from "react-router-dom";
 
 class CategoryPageState {
-    categories: { name: string; services: string[] }[] = [];
+    categories: CategoryModel[] = [];
+    errorMessage: string = "";
 }
 export default class CategoryPage extends BasePage<{}> {
     state: CategoryPageState;
@@ -11,7 +15,8 @@ export default class CategoryPage extends BasePage<{}> {
         super(props);
 
         this.state = {
-            categories: []
+            categories: [],
+            errorMessage: ""
         };
     }
 
@@ -19,42 +24,50 @@ export default class CategoryPage extends BasePage<{}> {
         this.getCategories();
     }
 
-    getCategories() {
-        this.setState({
-            categories: [
-                {
-                    categoryId: 1,
-                    name: "Prva kategorija",
-                    services: [
-                        "Usluga 1 prve kategorije",
-                        "Usluga 2 prve kategorije"
-                    ]
-                },
-                {
-                    categoryId: 2,
-                    name: "Druga kategorija",
-                    services: [
-                        "Usluga 1 druge kategorije",
-                        "Usluga 2 druge kategorije"
-                    ]
-                }
-            ]
+    private getCategories() {
+        axios({
+            method: "get",
+            baseURL: "http://localhost:9001",
+            url: "/category",
+            timeout: 10000,
+            headers: {
+                Authorization: "Bearer FAKE-TOKEN"
+            },
+            maxRedirects: 0
+        })
+        .then(res => {
+            if (!Array.isArray(res.data)) {
+                throw new Error("Invalid data received");
+            }
+            this.setState({
+                errorMessage: "",
+                categories: res.data
+            });
+        })
+        .catch(err => {
+            this.setState({
+                categories: [],
+                errorMessage: "Nije moguće učitati kategorije usluga klinike"
+            })
         });
     }
 
     renderMain(): JSX.Element {
         return (
             <>
+                { this.state.errorMessage.length > 0 && <p>{ this.state.errorMessage }</p> }
                 <h2>Kategorije usluga klinike</h2>
                 <ul>
                     {
                         this.state.categories.map(category => (
-                            <div key={category.name} style={{ border: "1px solid blue", borderRadius: "5px", padding: "5px", margin: "5px 0px 5px 0px" }}>
+                            <div key={category.categoryId} style={{ border: "1px solid blue", borderRadius: "5px", padding: "5px", margin: "5px 0px 5px 0px" }}>
                                 <h3>{category.name}</h3>
                                 <ul>
                                     {
                                         category.services.map(service => (
-                                            <p key={service}>{service}</p>
+                                            <Link to={`/service/${service.serviceId}`} key={service.serviceId}>
+                                                { service.name }
+                                            </Link>
                                         ))
                                     }
                                 </ul>
