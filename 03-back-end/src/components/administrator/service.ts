dfaeee451feb9443491c4e23c2d178401dc74394
Luka Gajić <45518706+lukagajic@users.noreply.github.com>
2021-06-01 14:28:28 +0200

    Replace delete logic inside administrator service

--- 03-back-end/src/components/administrator/service.ts
+++ 03-back-end/src/components/administrator/service.ts
@@ -101,34 +101,7 @@ class AdministratorService extends BaseService<AdministratorModel> {
     }
 
     public async delete(administratorId: number): Promise<IErrorResponse> {
-        return new Promise<IErrorResponse>(async resolve => {
-            const sql: string = "DELETE FROM administrator WHERE administrator_id = ?";
-
-            this.db.execute(sql, [ administratorId ])
-                .then(async result => {
-                    const deleteInfo: any = result[0];
-                    const deletedRowCount: number = +(deleteInfo?.affectedRows);
-
-                    if (deletedRowCount === 1) {
-                        resolve({
-                            errorCode: 0,
-                            errorMessage: "One record deleted!"
-                        });
-                    } else {
-                        resolve({
-                            errorCode: -1,
-                            errorMessage: "This record could not be deleted!"
-                        }); 
-                    }
-                })
-                .catch(error => {
-                    resolve({
-                        errorCode: error?.errno,
-                        errorMessage: error?.sqlMessage
-                    });
-                })
-
-        });
+        return await this.deleteByIdFromTable("administrator", administratorId);
     }
 
     public async getByUsername(username: string, options: Partial<AdministratorModelAdapterOptions> = {}): Promise<AdministratorModel | null> {
