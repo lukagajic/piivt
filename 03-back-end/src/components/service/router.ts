9f62e3293c2a1a61a3ba7c3b22ca437322096ad8
Luka Gajić <45518706+lukagajic@users.noreply.github.com>
2021-06-11 16:42:01 +0200

    Protect the service routes on backend

--- 03-back-end/src/components/service/router.ts
+++ 03-back-end/src/components/service/router.ts
@@ -2,16 +2,46 @@ import * as express from 'express';
 import ServiceController from './controller';
 import IApplicationResources from '../../common/IApplicationResources.interface';
 import IRouter from '../../common/IRouter.inteface';
+import AuthMiddleware from '../../middleware/auth.middleware';
 
 export default class ServiceRouter implements IRouter {
     public setupRoutes(application: express.Application, resources: IApplicationResources) {
         const serviceController: ServiceController = new ServiceController(resources);
 
-        application.get("/service/:id",             serviceController.getById.bind(serviceController));
-        application.get("/service",                 serviceController.getAll.bind(serviceController));
-        application.get("/category/:cid/service",   serviceController.getAllInCategory.bind(serviceController));
-        application.post("/service",                serviceController.add.bind(serviceController));
-        application.put("/service/:id",             serviceController.edit.bind(serviceController));
-        application.delete("/service/:id",          serviceController.deleteById.bind(serviceController));
+        application.get(
+            "/service/:id",
+            AuthMiddleware.getVerifier("doctor"),
+            serviceController.getById.bind(serviceController)
+        );
+
+        application.get(
+            "/service",
+            AuthMiddleware.getVerifier("doctor"),     
+            serviceController.getAll.bind(serviceController)
+        );
+        
+        application.get(
+            "/category/:cid/service",
+            AuthMiddleware.getVerifier("doctor"),
+            serviceController.getAllInCategory.bind(serviceController)
+        );
+
+        application.post(
+            "/service",
+            AuthMiddleware.getVerifier("doctor"),
+            serviceController.add.bind(serviceController)
+        );
+        
+        application.put(
+            "/service/:id",
+            AuthMiddleware.getVerifier("doctor"), 
+            serviceController.edit.bind(serviceController)
+        );
+
+        application.delete(
+            "/service/:id",
+            AuthMiddleware.getVerifier("doctor"), 
+            serviceController.deleteById.bind(serviceController)
+        );
     }
 }
